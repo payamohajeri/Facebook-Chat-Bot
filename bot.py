096c70199cc4da49c6802e24a3bb3348b88535a1
Payam Mohajeri <payam_p70@yahoo.com>
2016-11-18 14:05:23 +0100

    setup heroku, install gunicorn, add pyc files to gitignore, handle a syntax error in bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,26 @@
 #!/usr/local/bin/python
 
+import os
+import sys
+import json
 
-def main() {
+import requests
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def verify():
+    return "Hello world", 200
+
+def log(message):
+    print str(message)
+    sys.stdout.flush()
+
+def main():
     print("Bot")
-}
 
 if __name__ == '__main__':
-    main()
+    # main()
+    app.run(debug=True)
