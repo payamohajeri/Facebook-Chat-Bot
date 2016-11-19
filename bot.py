2844eb3f70c40e8a380ebcda0237b3ed51bde2c2
Payam Mohajeri <payam_p70@yahoo.com>
2016-11-19 15:59:52 +0100

    syntax error, change response class location

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def verify():
     #verify facebook callback url
-    if request.args.get("hub.mode") == "subscribe" and
+    if request.args.get("hub.mode") == "subscribe" and \
         request.args.get("hub.challenge"):
         if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
             return "Verification token mismatch", 403
