829e639be6b82ead70d82b957f933741936a74f0
Payam Mohajeri <payam_p70@yahoo.com>
2016-11-20 15:15:36 +0100

    bug - typo in attachment class

--- chatbot/attachment.py
+++ chatbot/attachment.py
@@ -1,7 +1,6 @@
 class attachment(object):
     def __init__(self, type):
         self.type=type
-        self.payload=None
 
     def setType(self, value):
         self.type=value
@@ -15,11 +14,16 @@ class attachment(object):
     def getPayload(self):
         return self.payload
 
+    def to_dict(self):
+        return {
+            'type': self.type,
+            'payload': self.payload
+        }
+
 class ImageAttachment(attachment):
     def __init__(self, url):
         super(ImageAttachment, self).__init__(type='image')
         self.url = url
-        sefl.setPayload()
 
     def setURL(self, value):
         self.url = url
@@ -27,20 +31,14 @@ class ImageAttachment(attachment):
     def getURL(self, value):
         return self.url
 
-    def setPayload(self):
-        self.payload={ 'url': self.url }
-
-    def to_dict(self):
-        return {
-            'type': self.type,
-            'payload': self.getPayload
-        }
+    @property
+    def payload(self):
+        return { 'url': self.url }
 
 class TemplateAttachment(attachment):
     def __init__(self, template):
         super(TemplateAttachment, self).__init__(type='template')
         self.template = template
-        self.setPayload()
 
     def setTemplate(self, value):
         self.template = value
@@ -48,13 +46,8 @@ class TemplateAttachment(attachment):
     def getTemplate(self, value):
         return self.template
 
-    def setPayload(self):
-        self.payload=self.template.to_dict()
-
-    def to_dict(self):
-        return {
-            'type': self.type,
-            'payload': self.getPayload
-        }
+    @property
+    def payload(self):
+        return self.template.to_dict()
         
         
